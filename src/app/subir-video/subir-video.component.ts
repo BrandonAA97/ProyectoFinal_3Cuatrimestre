580016c36ef55727fa512cb567dc386e6c2091bd
LucasBebek <bebeklucas7@gmail.com>
2023-11-07 12:06:15 -0300

    reproductor bonito

--- src/app/subir-video/subir-video.component.ts
+++ src/app/subir-video/subir-video.component.ts
@@ -17,21 +17,21 @@ export class SubirVideoComponent {
   videoUrl?: any;
   videoImg?: any;
 constructor(
-  private service: videosService, 
-  private mediaService: MediaService, 
+  private service: videosService,
+  private mediaService: MediaService,
   private fb: FormBuilder
   ){}
- 
+
   video: Videos = new Videos(0, '', '', '', '', '');
-   
+
 
   // private fileVideo = new File([""], "video", {type: "video/mp4"}).toString;
   // private fileImg = new File ([""], "image", {type: "image/"}).toString;
 
    capturarArchivo(event: any){
     const file = event.target.files[0]
-    
-    
+
+
     if(file){
       if(file.type === 'video/mp4'){
         this.videoUrl = file;
@@ -49,26 +49,26 @@ constructor(
     if(file !== undefined){
 
        const fileName = file.name;
-      
+
        const fileNameString = String(fileName);
 
        this.video.url = fileNameString;
        console.log(this.video.url)
     }
-       
+
       //  this.mediaService.subirFichero(fb)
       //       .subscribe(response=>{
       //         console.log('video subido', response);
       //       });
     }
-     
+
     procesarImage(file: File | undefined) {
       //const fb = new FormData();
       if(file !== undefined){
         const fileName = file.name;
-       
+
         const fileNameString = String(fileName);
- 
+
         this.video.image = fileNameString;
         console.log(this.video.image)
       }
@@ -77,8 +77,8 @@ constructor(
        //         console.log('video subido', response);
        //       });
      }
-  
-  
+
+
   subir(video:Videos){
     if(this.video.titulo && this.video.url && this.video.image && this.video.categoria && this.video.subtitulo ){
       //crea el formdata para almacenar el file con el video que trae this.video.url y lo guarda en "fbVideo"
@@ -89,10 +89,10 @@ constructor(
        .subscribe(response =>{
          console.log('video subido', response);
 
-         
+
          console.log("video.url: " + this.videoUrl)
         });
-    
+
     //crea el formdata para almacenar el file con el video que trae this.video.url y lo guarda en "fbImage"
     const fbImagen = new FormData();
     fbImagen.append('file', this.videoImg);
@@ -100,13 +100,13 @@ constructor(
        this.mediaService.subirFichero(fbImagen)
        .subscribe(response => {
          console.log('Imagen subida', response);
-        
-         
+
+
          console.log("video.image: " + this.videoImg)
         });
-      
+
       this.service.agregarVideo(video).subscribe(data =>{
-      alert("Video agregado!");
+      alert("Video Subido!");
       console.log("Video final:" + video.url, " Imagen final:" + video.image)
     });
     }else{
